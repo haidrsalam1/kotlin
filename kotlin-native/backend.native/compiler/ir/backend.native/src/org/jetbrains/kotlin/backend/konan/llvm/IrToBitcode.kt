3b9c4d87c9d9863b008d2d147c9b1f2aba9284d6
Svyatoslav Scherbina <svyatoslav.scherbina@jetbrains.com>
2021-08-17 16:57:43 +0300

    Native: allow subclassing FunctionGenerationContext
    This allows one to have domain-specific generator without polluting
    the global namespace.
    
    Also introduce builders and do minor refactoring.

--- kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
+++ kotlin-native/backend.native/compiler/ir/backend.native/src/org/jetbrains/kotlin/backend/konan/llvm/IrToBitcode.kt
@@ -366,7 +366,7 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
 
         context.llvm.initializersGenerationState.globalInitFunction?.let { fileInitFunction ->
             generateFunction(codegen, fileInitFunction, null, null) {
-                using(FunctionScope(fileInitFunction, it)) {
+                using(FunctionScope(fileInitFunction, this)) {
                     val parameterScope = ParameterScope(fileInitFunction, functionGenerationContext)
                     using(parameterScope) usingParameterScope@{
                         using(VariableScope()) usingVariableScope@{
@@ -383,7 +383,7 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
 
         context.llvm.initializersGenerationState.threadLocalInitFunction?.let { fileInitFunction ->
             generateFunction(codegen, fileInitFunction, null, null) {
-                using(FunctionScope(fileInitFunction, it)) {
+                using(FunctionScope(fileInitFunction, this)) {
                     val parameterScope = ParameterScope(fileInitFunction, functionGenerationContext)
                     using(parameterScope) usingParameterScope@{
                         using(VariableScope()) usingVariableScope@{
@@ -470,7 +470,7 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
         )
         LLVMSetLinkage(initFunction, LLVMLinkage.LLVMPrivateLinkage)
         generateFunction(codegen, initFunction) {
-            using(FunctionScope(initFunction, "init_body", it)) {
+            using(FunctionScope(initFunction, "init_body", this)) {
                 val bbInit = basicBlock("init", null)
                 val bbLocalInit = basicBlock("local_init", null)
                 val bbLocalAlloc = basicBlock("local_alloc", null)
@@ -830,7 +830,7 @@ internal class CodeGeneratorVisitor(val context: Context, val lifetimes: Map<IrE
             generateFunction(codegen, declaration,
                     declaration.location(start = true),
                     declaration.location(start = false)) {
-                using(FunctionScope(declaration, it)) {
+                using(FunctionScope(declaration, this)) {
                     val parameterScope = ParameterScope(declaration, functionGenerationContext)
                     using(parameterScope) usingParameterScope@{
                         using(VariableScope()) usingVariableScope@{
